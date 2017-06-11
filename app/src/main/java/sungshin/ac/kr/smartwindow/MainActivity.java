b13fc65a1024b7d25b228b3906b7a84ed9756447
hyeona <gusdk7656@naver.com>
2017-06-11 17:55:35 +0900

    hyeona1

---
+++ app/src/main/java/sungshin/ac/kr/smartwindow/MainActivity.java
@@ -0,0 +1,13 @@
+package sungshin.ac.kr.smartwindow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
