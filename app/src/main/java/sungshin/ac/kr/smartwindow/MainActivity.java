01b976c3a50d394737fbe52a330757da8629a5a7
hyeona <gusdk7656@naver.com>
2017-06-19 00:34:53 +0900

    firebase example

--- app/src/main/java/sungshin/ac/kr/smartwindow/MainActivity.java
+++ app/src/main/java/sungshin/ac/kr/smartwindow/MainActivity.java
@@ -1,13 +1,22 @@
 package sungshin.ac.kr.smartwindow;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //추가한 라인
+        //주제별로 회원을 선택해서 알림을 보내려는 코드
+        //news 라는 주제를 가진 사람에게 알림을 보내는 코드
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        FirebaseInstanceId.getInstance().getToken();
+
+
     }
 }
---
+++ app/src/main/java/sungshin/ac/kr/smartwindow/service/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,52 @@
+package sungshin.ac.kr.smartwindow.service.fcm;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+import sungshin.ac.kr.smartwindow.MainActivity;
+import sungshin.ac.kr.smartwindow.R;
+
+/**
+ * Created by Sansung on 2017-06-19.
+ */
+
+public class MyFirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+    private static final String TAG = "FirebaseMsgService";
+
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        //추가한것
+        sendNotification(remoteMessage.getData().get("message"));
+    }
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        //아이콘이나 제목 수정 -> 알림의 모양을 설정하는 코드
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("FCM Push Test")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
