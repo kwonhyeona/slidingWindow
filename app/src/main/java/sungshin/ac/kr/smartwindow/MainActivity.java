0d9679ed5fc4d9dbadb4224e6745f24f15dbd3e7
sophia.k <sophia.k@kakaocorp.com>
2017-08-05 16:02:07 +0900

    푸쉬 에러해

--- app/src/main/java/sungshin/ac/kr/smartwindow/MainActivity.java
+++ app/src/main/java/sungshin/ac/kr/smartwindow/MainActivity.java
@@ -2,7 +2,9 @@ package sungshin.ac.kr.smartwindow;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
@@ -15,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
         //추가한 라인
         //주제별로 회원을 선택해서 알림을 보내려는 코드
         //news 라는 주제를 가진 사람에게 알림을 보내는 코드
-        FirebaseMessaging.getInstance().subscribeToTopic("news");
-        FirebaseInstanceId.getInstance().getToken();
+//        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        FirebaseApp.initializeApp(this);
+        Log.d("MainActivity", "Token : " + FirebaseInstanceId.getInstance().getToken());
 
     }
 
